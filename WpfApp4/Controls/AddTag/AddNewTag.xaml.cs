560774ab813797a3647d4aee4231f29249c9d35b
Yishai Neeman <yishai@om2.com>
2019-06-28 14:08:29 +0300

    add new tag to group

--- WpfApp4/Controls/AddTag/AddNewTag.xaml.cs
+++ WpfApp4/Controls/AddTag/AddNewTag.xaml.cs
@@ -22,15 +22,20 @@ namespace WpfApp4.Controls.AddTag
     /// </summary>
     public partial class AddNewTag : Window
     {
+        private RadioButton quickReportCalendarCheckedRadioButton;
+        ObservableCollection<tagsCategory> categories;
         public AddNewTag(ObservableCollection<tagsCategory> cats)
         {
             InitializeComponent();
             cbCats.MyItemsSource = cats;
+            categories = cats;
         }
 
         private void btnDialogOk_Click(object sender, RoutedEventArgs e)
         {
+            CheckRadioButton();
             this.DialogResult = true;
+
         }
 
         private void Window_ContentRendered(object sender, EventArgs e)
@@ -38,6 +43,29 @@ namespace WpfApp4.Controls.AddTag
 
         }
 
-      
+        private void CheckRadioButton()
+        {
+            if(quickReportCalendarCheckedRadioButton.Content.ToString()=="New")
+            {
+                if (!categories.Any(p => p.categoryName == mainCat.Text))
+                    categories.Add(new tagsCategory(mainCat.Text, new List<string> { subCat.Text }, false));
+            }
+            else
+            {
+                var selectedItem = (tagsCategory)cbCats.MySelectedItem;
+                selectedItem.categoryOptions.Add(newOption.Text);
+            }
+        }
+
+        private void radioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton ck = sender as RadioButton;
+            if (ck.IsChecked.Value)
+                quickReportCalendarCheckedRadioButton = ck;
+        }
+
+
+
+
     }
 }
--- WpfApp4/Controls/TagControl/andOrSelection.xaml.cs
+++ WpfApp4/Controls/TagControl/andOrSelection.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,11 +22,16 @@ namespace WpfApp4.Controls.TagControl
     /// </summary>
     public partial class andOrSelection : Window
     {
+        ObservableCollection<string> LVFilterTags = new ObservableCollection<string>() ;
+        string mainCatText = "CategoryName=";
+        string subCatText = "Value=";
 
         public andOrSelection(ObservableCollection<tagsCategory> cats)
         {
             InitializeComponent();
+            LVFilterTags.CollectionChanged += this.OnCollectionChanged;
             x.ItemsSource = cats;
+            paramLV.ItemsSource = LVFilterTags;
 
         }
         private void btnDialogOk_Click(object sender, RoutedEventArgs e)
@@ -40,10 +46,80 @@ namespace WpfApp4.Controls.TagControl
 
         private void addParamtoLV(object sender, RoutedEventArgs e)
         {
-
-            string paramsSelection = x.SelectedValue.ToString() + "." + x1.SelectedValue.ToString();
-            if(!paramLV.Items.Contains(paramsSelection))
-            paramLV.Items.Add(paramsSelection);
+            
+            string paramsSelection = mainCatText + x.SelectedValue.ToString() +","+ subCatText + x1.SelectedValue.ToString();
+            if(!LVFilterTags.Contains(paramsSelection))
+                LVFilterTags.Add(paramsSelection);
         }
+
+        void OnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            List<Tags.TagFilter> list1 = new List<Tags.TagFilter>();
+
+            foreach(string param in (ObservableCollection<string>)sender)
+            {
+                var list = param.Split(',');
+                list[0] = list[0].Replace(mainCatText, string.Empty);
+                list[1] = list[1].Replace(subCatText, string.Empty);
+                list1.Add(new Tags.TagFilter(list[0], list[1]));
+            }
+
+            var tagsOrder = (from listItem in list1
+                             group listItem by listItem.path into g
+                             select Merge(g)
+      ).ToList();
+
+            string andOrLabel=string.Empty;
+            bool isFirst = true;
+            foreach(TagFilter tf in tagsOrder)
+            {
+                var l = tf.FileTag.Split(';').ToList();
+                if (l.Count() == 1)
+                {
+                    if (isFirst)
+                        andOrLabel += tf.path + "=" + l[0];
+                    else
+                        andOrLabel += " AND " + tf.path + "=" + l[0];
+                }
+                else
+                {
+                    foreach(string item in l)
+                    {
+                        if (isFirst)
+                        {
+                            andOrLabel += tf.path + "=" + item;
+                            isFirst = false;
+                        }
+                            
+                        else
+                            andOrLabel += " AND " + tf.path + "=" + item;
+                    }
+                }
+                    
+
+                if(isFirst)
+                    isFirst=false;
+            }
+            paramsText.Text = andOrLabel;
+
+
+
+        }
+
+        private Tags.TagFilter Merge( IEnumerable<Tags.TagFilter> paths)
+        {
+            Tags.TagFilter u = new Tags.TagFilter("", "");
+            if (!paths.Any())
+            {
+                return u;
+            }
+            else
+            {
+                u.path = paths.First().path;
+                u.FileTag = string.Join(";", paths.Select(x => x.FileTag));
+                return u;
+            }
+        }
+
     }
 }
